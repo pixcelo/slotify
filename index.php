607ab1ba91fccb7d528eb88d0d88e24d8e0a099c
tetsu-upstr <bodomu@gmail.com>
2020-05-24 17:55:13 +0900

    ログインフォーム

--- index.php
+++ index.php
@@ -3,9 +3,25 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>slotify</title>
 </head>
 <body>
-  hello wor;d
+
+  <div id="inputContainer">
+    <form id="loginForm" action="register.php" method="POST">
+      <h2>ログイン</h2>
+      <p>
+      <label for="loginUsername">氏名</label>
+        <input id="loginUsername" name="loginUsername" type="text" placeholder="名前を入力してください" required>
+      </p>
+      <p>
+        <label for="loginPassword">パスワード</label>
+        <input id="loginPassword" name="loginPassword" type="password" required>
+      </p>
+
+      <button type="submit" name="loginButton">ログイン</button>
+    </form>
+  </div>
+  
 </body>
 </html>
