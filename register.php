e8891ab9a7bea5d39e871e38452dd32492bded9d
tetsu-upstr <bodomu@gmail.com>
2020-05-30 17:13:48 +0900

    register.css

--- register.php
+++ register.php
@@ -23,71 +23,77 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>slotify</title>
+  <link rel="stylesheet" href="assets/css/register.css">
 </head>
 <body>
+  <div id="background">
 
-  <div id="inputContainer">
-    <form id="loginForm" action="register.php" method="POST">
-      <h2>ログイン</h2>
-      <p>
-      <?php echo $account->getError(Constants::$loginFailed); ?>
-      <label for="loginUsername">ユーザーネーム</label>
-        <input id="loginUsername" name="loginUsername" type="text" placeholder="ユーザーネーム" required>
-      </p>
-      <p>
-        <label for="loginPassword">パスワード</label>
-        <input id="loginPassword" name="loginPassword" type="password" placeholder="パスワード" required>
-      </p>
+    <div id="loginContainer">
 
-      <button type="submit" name="loginButton">ログイン</button>
-    </form>
+        <div id="inputContainer">
+          <form id="loginForm" action="register.php" method="POST">
+            <h2>アカウントでログイン</h2>
+            <p>
+            <?php echo $account->getError(Constants::$loginFailed); ?>
+            <label for="loginUsername">ユーザーネーム</label>
+              <input id="loginUsername" name="loginUsername" type="text" placeholder="例：たっくん" required>
+            </p>
+            <p>
+              <label for="loginPassword">パスワード</label>
+              <input id="loginPassword" name="loginPassword" type="password" placeholder="あなたのパスワード" required>
+            </p>
 
-    <form id="registerForm" action="register.php" method="POST">
-      <h2>アカウント新規作成</h2>
-      <p>
-      <!-- エラーがあればAccountクラスのエラーメッセージを表示する -->
-      <?php echo $account->getError(Constants::$userNameCharacters); ?>
-      <?php echo $account->getError(Constants::$usernameTaken); ?>
-      <label for="username">ユーザーネーム</label>
-        <input id="username" name="username" type="text" placeholder="たっくん" value="<?php getInputValue('username') ?>" required>
-      </p>
-      <p>
-      <?php echo $account->getError(Constants::$firstNameCharacters); ?>
-      <label for="firstName">名前</label>
-        <input id="firstName" name="firstName" type="text" placeholder="太郎" value="<?php getInputValue('firstName') ?>" required>
-      </p>
-      <p>
-      <?php echo $account->getError(Constants::$lastNameCharacters); ?>
-      <label for="lastName">性別</label>
-        <input id="lastName" name="lastName" type="text" placeholder="田中" value="<?php getInputValue('lastName') ?>" required>
-      </p>
-      <p>
-      <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
-      <?php echo $account->getError(Constants::$emailInvalid); ?>
-      <?php echo $account->getError(Constants::$emailTaken); ?>
-      <label for="email">メールアドレス</label>
-        <input id="email" name="email" type="email" placeholder="test@test.com" value="<?php getInputValue('email') ?>" required>
-      </p>
-      <p>
-      <label for="email2">メールアドレス確認用</label>
-        <input id="email2" name="email2" type="email" placeholder="test@test.com" value="<?php getInputValue('email2') ?>" required>
-      </p>
-      <p>
-        <?php echo $account->getError(Constants::$passwordsDoNomatch); ?>
-        <?php echo $account->getError(Constants::$passwordsNotAlphanumeric); ?>
-        <?php echo $account->getError(Constants::$passwordsCharacters); ?>
-        <label for="password">パスワード</label>
-        <input id="password" name="password" type="password" placeholder="パスワード" required>
-      </p>
-      <p>
-        <label for="password2">パスワード確認用</label>
-        <input id="password2" name="password2" type="password" placeholder="パスワード" required>
-      </p>
+            <button type="submit" name="loginButton">ログイン</button>
+          </form>
 
+          <form id="registerForm" action="register.php" method="POST">
+            <h2>アカウント新規登録</h2>
+            <p>
+            <!-- エラーがあればAccountクラスのエラーメッセージを表示する -->
+            <?php echo $account->getError(Constants::$userNameCharacters); ?>
+            <?php echo $account->getError(Constants::$usernameTaken); ?>
+            <label for="username">ユーザーネーム</label>
+              <input id="username" name="username" type="text" placeholder="例：たっくん" value="<?php getInputValue('username') ?>" required>
+            </p>
+            <p>
+            <?php echo $account->getError(Constants::$firstNameCharacters); ?>
+            <label for="firstName">名前</label>
+              <input id="firstName" name="firstName" type="text" placeholder="例：太郎" value="<?php getInputValue('firstName') ?>" required>
+            </p>
+            <p>
+            <?php echo $account->getError(Constants::$lastNameCharacters); ?>
+            <label for="lastName">性別</label>
+              <input id="lastName" name="lastName" type="text" placeholder="例：田中" value="<?php getInputValue('lastName') ?>" required>
+            </p>
+            <p>
+            <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+            <?php echo $account->getError(Constants::$emailInvalid); ?>
+            <?php echo $account->getError(Constants::$emailTaken); ?>
+            <label for="email">メールアドレス</label>
+              <input id="email" name="email" type="email" placeholder="例：test@gmail.com" value="<?php getInputValue('email') ?>" required>
+            </p>
+            <p>
+            <label for="email2">メールアドレス確認</label>
+              <input id="email2" name="email2" type="email" placeholder="例：test@gmail.com" value="<?php getInputValue('email2') ?>" required>
+            </p>
+            <p>
+              <?php echo $account->getError(Constants::$passwordsDoNomatch); ?>
+              <?php echo $account->getError(Constants::$passwordsNotAlphanumeric); ?>
+              <?php echo $account->getError(Constants::$passwordsCharacters); ?>
+              <label for="password">パスワード</label>
+              <input id="password" name="password" type="password" placeholder="あなたのパスワード" required>
+            </p>
+            <p>
+              <label for="password2">パスワード確認</label>
+              <input id="password2" name="password2" type="password" placeholder="あなたのパスワード" required>
+            </p>
 
-      <button type="submit" name="registerButton">登録</button>
-    </form>
-  </div>
-  
+            <button type="submit" name="registerButton">登録</button>
+          </form>
+
+        </div><!-- id="inputContainer" -->
+    </div><!-- id="loginContainer" -->
+  </div><!-- id="background" -->
+
 </body>
 </html>
