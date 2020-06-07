4017922aaff01a672988a413e7796789dbb088ef
tetsu-upstr <bodomu@gmail.com>
2020-06-08 04:33:12 +0900

    play button

--- album.php
+++ album.php
@@ -46,7 +46,7 @@ $artist = $album->getArtist();
       
       echo "<li class='trackListRow'>
               <div class='trackCount'>
-                  <img class='play' src='assets/images/icons/play-white.png'>
+                  <img class='play' src='assets/images/icons/play-white.png' onclick='setTrack(\™" . $albumSong->getId() ."\", tempPlaylist, true)'>
                   <span class='trackNumber'>$i</span>
               </div>
 
@@ -68,6 +68,12 @@ $artist = $album->getArtist();
     }
 
     ?>
+
+    <script>
+      let tempSongIds = <?php echo json_encode($songIdArray); ?>
+      tempPlaylist = JSON.parse(tempSongIds);
+
+    </script>
 
   </ul>
 </div>
