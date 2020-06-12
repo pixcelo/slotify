3be156683bf4fdaad9fa753a846e80c81e476080
tetsu-upstr <bodomu@gmail.com>
2020-06-12 14:34:50 +0900

    create User class

--- includes/includedFiles.php
+++ includes/includedFiles.php
@@ -5,9 +5,18 @@
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
     // Ajaxで遷移したらヘッダーとフッター以外を入れ替える
     include('includes/config.php');
+    include('includes/classes/User.php');
     include('includes/classes/Artist.php');
     include('includes/classes/Album.php');
     include('includes/classes/Song.php');
+
+    if (isset($_GET['userLoggedIn'])) {
+        $userLoggedIn = new User($con, $_GET['userLoggedIn']);
+    } else {
+        echo "usernameの変数が通っていません。JSのopenPageメソッドを確認してください";
+        exit();
+    }
+
 } else {
     include("includes/header.php");
     include("includes/footer.php");
