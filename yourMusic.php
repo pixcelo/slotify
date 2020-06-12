c0221b99116e2c7c6e25e6054763a28f45be17c8
tetsu-upstr <bodomu@gmail.com>
2020-06-12 15:03:28 +0900

    playlist link & styling

--- yourMusic.php
+++ yourMusic.php
@@ -22,9 +22,9 @@ include("includes/includedFiles.php");
 
           while($row = mysqli_fetch_array($playlistQuery)) {
 
-            $playlist = new Playlist($con, $row);
+            $playlist = new Playlist($con, $row); 
 
-            echo "<div class='gridViewItem'>
+            echo "<div class='gridViewItem' role='link' tabindex='0' onclick='openPage(\"playlist.php?id=" . $playlist->getId() . "\")'>
 
                       <div class='playlistImage'>
                         <img src='assets/images/icons/playlist.png'>
