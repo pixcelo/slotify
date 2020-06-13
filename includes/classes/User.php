1a691797be0d8e7f1f26cc42b60776a02b1a0b5a
tetsu-upstr <bodomu@gmail.com>
2020-06-14 07:08:10 +0900

    setting.php

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -13,6 +13,12 @@
         return $this->username;
     }
 
+    public function getFirstAndLastName() {
+          // concat関数は引数を連結する
+          $query = mysqli_query($this->con, "SELECT concat(firstName, ' ', lastName) as 'name' FROM users WHERE username='$this->username'");
+          $row = mysqli_fetch_array($query);
+          return $row['name'];
+    }
     
 
   }
