0b9d8169c324f72c8a58a028f306cb9ccf012bad
tetsu-upstr <bodomu@gmail.com>
2020-06-14 11:55:24 +0900

    updatePassword function

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,6 +54,18 @@ function updateEmai(emailClass) {
     });
 }
 
+function updatePassword(odlPasswordClass, newPasswordClass1, newPasswordClass2) {
+    var oldPassword = $("." + odlPasswordClass).val();
+    var newPasswordClass1 = $("." + newPasswordClass1).val();
+    var newPasswordClass2 = $("." + newPasswordClass2).val();
+
+    $.post("includes/handlers/ajax/updatePassword.php", 
+    { oldPassword: oldPassword, newPassword1: newPassword1, newPassword2: newPassword2, username: userLoggedIn})
+    .done(function(response) {
+        $("." + odlPasswordClass).nextAll(".message").text(response);
+    });
+}
+
 function logout() {
     $.post("includes/handlers/ajax/logout.php", function() {
         location.reload();
